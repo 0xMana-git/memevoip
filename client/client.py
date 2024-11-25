bc4e3dd0264eb0541f056ed8e4f178cdda03a002
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 03:57:23 -0800

    fix client ssl

--- client/client.py
+++ client/client.py
@@ -1,15 +1,15 @@
 import ssl
 import os
 import threading
-
+import socket
 
 fifo_in_path = "audio_in"
 fifo_out_path = "audio_out"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(10)
+sock_raw = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_raw.settimeout(10)
 
-host, port = "mana.kyun.li", "14880"
+HOST, PORT = "mana.kyun.li", 14880
 pipe_paths = "pipes/"
 key_base = "/etc/letsencrypt/live/mana.kyun.li"
 keyfile = key_base + "/privkey.pem"
@@ -17,25 +17,34 @@ certfile = key_base + "/fullchain.pem"
 #4kb
 buffer_size = 1024 * 4
 muxout_path = "muxed_out"
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-client = context.wrap_socket(sock)
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+sock = context.wrap_socket(sock_raw)
 
 
 def send_thread(fifo):
     while True:
         buffer_send = fifo.read(buffer_size)
         sock.send(buffer_send)
+        print("sent bufsize")
 def recv_thread(fifo):
     while True:
         buffer_recv = sock.recv(buffer_size)
         fifo.write(buffer_recv)
+        print("recv bufsize")
+
 def main():
     fifo_in = open(fifo_in_path, "rb")
     fifo_out = open(fifo_out_path, "wb")
-    sock.connect((host, port))
-    st = threading.Thread(send_thread, (fifo_in))
-    rt = threading.Thread(recv_thread, (fifo_out))
+    print("Connecting to host")
+    sock.connect((HOST, PORT))
+    print("Connected")
+    st = threading.Thread(target=send_thread, args=(fifo_in,))
+    rt = threading.Thread(target=recv_thread, args=(fifo_out,))
     st.start()
     rt.start()
     st.join()
     rt.join()
+
+
+if __name__ == "__main__":
+    main()
--- client/client.sh
+++ client/client.sh
@@ -2,7 +2,7 @@ rm audio_in
 rm audio_out
 mkfifo audio_in
 mkfifo audio_out
-ffmpeg -y -f pulse -sample_rate 44100 -channels 1 -i hw:0 -f wav audio_in &
+ffmpeg -y -f pulse -sample_rate 44100 -channels 1 -i hw:0 -f wav audio_in 2>/dev/null &
 python client.py &
 mpv audio_out
 
