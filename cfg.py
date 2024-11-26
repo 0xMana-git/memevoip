6320efd048db8d631ebd891bb7bb32b8649aa703
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 17:04:48 -0800

    meds

--- cfg.py
+++ cfg.py
@@ -3,4 +3,4 @@ buffer_size = 1024 * 4
 fifo_pipes_root = "/tmp/memevoip/"
 server_sleep_time = 3
 #ffmpeg_client_in = "-i /home/mana/Music/meme.opus".split(" ")
-ffmpeg_client_in = "-f pulse -i hw:2".split(" ")playback_command = "aplay -f cd".split(" ")
+ffmpeg_client_in = "-f pulse -i hw:2".split(" ")playback_command = "aplay -f cd".split(" ")playback_command = "aplay -f cd".split(" ")
