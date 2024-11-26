ac3da6501fb7300367f6dd8c771035c2b08d1358
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 19:27:03 -0800

    fix

--- shell_client_dummy.sh
+++ shell_client_dummy.sh
@@ -1,4 +1,5 @@
 rm dummy_in
 mkfifo dummy_in
-bash $(dirname "$0")/shell_client_internal.sh /home/mana/Music/meme.opus dummy_in /dev/null
+bash $(dirname "$0")/shell_client_internal.sh /home/mana/Music/meme.opus dummy_in /dev/null &
+sleep infinity
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
