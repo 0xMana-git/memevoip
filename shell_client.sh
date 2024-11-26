edffb38634feb281606c3f67ef053f414b3371c9
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 19:50:08 -0800

    wtf

--- shell_client.sh
+++ shell_client.sh
@@ -2,6 +2,6 @@ rm audio_in
 rm audio_out
 mkfifo audio_in
 mkfifo audio_out
+bash $(dirname "$0")/shell_client_internal.sh placeholder audio_in audio_out &
 mpv audio_out
-bash $(dirname "$0")/shell_client_internal.sh $1 audio_in audio_out &
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
