e4bf71cf67c02fc8d0ad1a2eea92898d8cdd480c
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 19:37:42 -0800

    mpv works o:

--- shell_client.sh
+++ shell_client.sh
@@ -3,5 +3,5 @@ rm audio_out
 mkfifo audio_in
 mkfifo audio_out
 bash $(dirname "$0")/shell_client_internal.sh /home/mana/Music/meme.opus audio_in audio_out &
-aplay -f cd audio_out
+mpv audio_out
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
--- shell_client_internal.sh
+++ shell_client_internal.sh
@@ -1,5 +1,6 @@
 #pipe in
-ffmpeg -y  -i $1 -ar 44100 -ac 2 -f wav $2 2>/dev/null &
+args=($1)
+ffmpeg -y  -i "${args[@]}" -ar 44100 -ac 2 -f wav $2 2>/dev/null &
 #pipe ncat
 socat STDIO OPENSSL-CONNECT:mana.kyun.li:14880 < $2 > $3
 
