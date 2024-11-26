b04dfe9c95662283b665969e9de70fa8c2c23531
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-25 21:39:33 -0800

    add connecting thing

--- shell_client_internal.sh
+++ shell_client_internal.sh
@@ -2,6 +2,7 @@
 ffmpeg -y $1 -ar 44100 -ac 2 -f wav $2 2>/dev/null &
 #pw-record --rate 44100 --target 145 $2 &
 #pipe ncat
+echo "Connecting..."
 socat STDIO OPENSSL-CONNECT:mana.kyun.li:14880 < $2 > $3
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
