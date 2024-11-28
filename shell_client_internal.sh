256fc35096ae1ceaef4c6ce6dfbf12711048429f
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-28 14:21:28 -0800

    thijg

--- shell_client_internal.sh
+++ shell_client_internal.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #pipe in
-ffmpeg -y $1 -ar 44100 -ac 2 -f wav $2 2>/dev/null &
-#ffmpeg -y $1 -ar 44100 -ac 2 -f wav $2 2>/dev/null &
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-
+ffmpeg -y $1 -ar 44100 -ac 2 -f wav $2 2>/dev/null & 
+#cat $1 > $2 &
 echo "Connecting..."
 socat STDIO OPENSSL-CONNECT:mana.kyun.li:14880 < $2 > $3
 
