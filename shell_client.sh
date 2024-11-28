36d22686ed241465a148e6b894514558f5261c96
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-28 14:29:14 -0800

    fix trap here

--- shell_client.sh
+++ shell_client.sh
@@ -2,7 +2,10 @@ rm audio_in
 rm audio_out
 mkfifo audio_in
 mkfifo audio_out
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 args="${@:1}"
 bash $(dirname "$0")/shell_client_internal.sh "$args" audio_in audio_out &
 mpv audio_out
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
