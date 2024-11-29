f085dd82ba1758c635d5a8f542cfaca82ba38860
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-28 17:45:28 -0800

    i wanna hang myself

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ def start_mux(clients : list, muxin_base_path : str, muxout_path : str) -> None:
     for i in range(inputs_len):
         filter_command += f"[a{i}]"
         
-    filter_command += f"amix=inputs={inputs_len}"
+    filter_command += f"amix=inputs={inputs_len}:duration=longest"
     command += [filter_command]
     #audio channel out
     command += ["-ac", "2"]
