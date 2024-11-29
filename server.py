1d10d00dbc030aa868295ea258ab6d160727b6f8
Mana <57663038+0xMana-git@users.noreply.github.com>
2024-11-28 16:19:45 -0800

    Update server.py

--- server.py
+++ server.py
@@ -58,8 +58,8 @@ def start_mux(clients : list, muxin_base_path : str, muxout_path : str) -> None:
             if i < len(filter_command) - 1:
                 filter_command += ",apad"
             filter_command += ";"
-            for i in range(len(clients)):
-                filter_command += f"[a{i}]"
+        for i in range(len(clients)):
+            filter_command += f"[a{i}]"
         
         filter_command += f"amerge={len(clients)}"
         command += [filter_command]
